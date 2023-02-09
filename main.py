3327418e8d36e3a0a1a674e1a0ece26def48659c
creamsandwch <neuronrus1996@gmail.com>
2023-02-09 14:43:49 +0500

    github commit

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import logging
 import sys
-import time
 
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from dotenv import load_dotenv
 
 from function_modules import item_funcs
-from bot_settings import settings
 
 
 log_format = (
@@ -58,6 +56,17 @@ def send_msg(bot: telegram.Bot, message: str, chat_id: str = ADMIN_CHAT_ID):
         logging.error(f'{error}: ошибка при отправке сообщения ботом.')
 
 
+def send_item_price(update, context):
+    """Команда запроса цены предмета по его имени."""
+    chat = update.effective_chat
+    try:
+        item_name = ' '.join(context.args)
+        item_price_response = item_funcs.get_item_avgprice_by_name(item_name)
+        context.bot.send_message(chat_id=chat.id, text=item_price_response)
+    except (IndexError, ValueError):
+        context.bot.send_message('There is no item name to search for.')
+
+
 def main():
     """Основная логика."""
     if check_tokens():
@@ -74,20 +83,7 @@ def main():
             Filters.text, standard_response
         )
     )
-
     updater.start_polling()
-    time.sleep(settings.RETRY_PERIOD)
-
-
-def send_item_price(update, context):
-    """Команда запроса цены предмета по его имени."""
-    chat = update.effective_chat
-    try:
-        item_name = ' '.join(context.args)
-        item_price_response = item_funcs.get_item_avgprice_by_name(item_name)
-        context.bot.send_message(chat_id=chat.id, text=item_price_response)
-    except (IndexError, ValueError):
-        context.bot.send_message('There is no item name to search for.')
 
 
 if __name__ == '__main__':
